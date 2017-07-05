f069de7ac44454cfba0744b153ef54af0204c664
baron-Endeavor <baronysyong@gmail.com>
2017-07-05 16:14:57 +0900

    server.py: Add print type

--- msgpackrpc/prototype/server.py
+++ msgpackrpc/prototype/server.py
@@ -3,7 +3,7 @@ import msgpackrpc
 class Something(object):
     def ListTest(self, name, position):
         for i in range(len(name)):
-            print name[i], position[i]
+            print type(name[i]), name[i], type(position[i]), position[i]
 
 server = msgpackrpc.Server(Something())
 server.listen(msgpackrpc.Address("", 18800))
